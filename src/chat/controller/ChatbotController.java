3f0f89f1531f53dfaac4634f0f9f254a456d5b6d
Van hoffman <zvan1276@740s-pl-119.local>
2018-10-12 09:51:55 -0600

    added jokes, and a while loop to quit

--- src/chat/controller/ChatbotController.java
+++ src/chat/controller/ChatbotController.java
@@ -1,12 +1,19 @@
 package chat.controller;
 
+import javax.swing.JOptionPane;
+
 import chat.model.ChatModel;
 
 public class ChatbotController
 {
+	
 	public void start()
 	{
-		
+		String userText = "...";
+		while (!userText.equalsIgnoreCase("quit"))
+		{
+			userText = JOptionPane.showInputDialog(null, "Type \"quit\" to exit.");
+		}
 	}
 	
 	ChatModel simplebot = new ChatModel();
--- src/chat/model/ChatModel.java
+++ src/chat/model/ChatModel.java
@@ -1,6 +1,9 @@
 package chat.model;
 
 import java.util.ArrayList;
+
+import javax.swing.JOptionPane;
+
 public class ChatModel
 {
 	ArrayList<String> responseList = new ArrayList();
@@ -11,10 +14,21 @@ public class ChatModel
 		
 	}
 	
-	private String joke()
+	private String jokeList()
 	{
+		String response = JOptionPane.showInputDialog(null, "ligma");
+		response = JOptionPane.showInputDialog(null, "The polar bears are dying. The ice caps are melting");
+		response = JOptionPane.showInputDialog(null, "your face");
+		response = JOptionPane.showInputDialog(null, "                ");
+		response = JOptionPane.showInputDialog(null, "The fitness gram pacer test is a multi-staged aerobic exercise.");
+		response = JOptionPane.showInputDialog(null, "Your MOMMMM");
+		
+		
 		
 	}
 	
-	
+	public String toString()
+	{
+		
+	}
 }
